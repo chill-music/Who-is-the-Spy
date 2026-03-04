864b134082539d683c4fee1adc81c87550b74d38
chillmusic <37777162+chill-music@users.noreply.github.com>
2026-03-04 02:39:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -88,6 +88,8 @@ const SHOP_ITEMS = {
         { id: 'frame_neon', name_en: "Neon Frame", name_ar: "إطار نيون", cost: 300, type: 'frames', preview: 'linear-gradient(45deg, #00f2ff, #7000ff)' },
         { id: 'frame_fire', name_en: "Fire Frame", name_ar: "إطار نار", cost: 400, type: 'frames', preview: 'linear-gradient(45deg, #ff0055, #ff8800)' },
         { id: 'frame_img', name_en: "Image Frame", name_ar: "إطار صورة", cost: 100, type: 'frames', preview: 'https://i.ibb.co/mVQTLr2D/Untitled-3.png' },
+        { id: 'test', name_en: "test1", name_ar: "إطار صورة", cost: 0, type: 'frames', preview: 'https://i.ibb.co/JwG3nhmZ/From-Klick-Pin-CF-Pin-on-artistic-cuts.gif' },
+
     ],
     titles: [
         { id: 'title_spy', name_en: "Mr. Spy", name_ar: "سيد جاسوس", cost: 600, type: 'titles' },
@@ -2090,4 +2092,5 @@ function App() {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
 
