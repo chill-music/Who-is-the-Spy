3588558d2e5c11f3d6d40a670c67513c7d9b5e65
chillmusic <37777162+chill-music@users.noreply.github.com>
2026-03-11 01:17:48 +0200

    Update 16-main.js

--- src/16-main.js
+++ src/16-main.js
@@ -4,6 +4,4 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<AppWithErrorBoundary />);
 
 // ⚡ Hide loading screen as soon as React renders
-if (typeof window._hideLoader === 'function') {
-    window._hideLoader();
-}
+if (typeof window._hideLoader === 'function') window._hideLoader();
