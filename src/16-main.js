4ae2639f9eef232364269881ef762ed07d758b62
chillmusic <37777162+chill-music@users.noreply.github.com>
2026-03-27 20:29:11 +0200

    Delete src directory

--- src/16-main.js
+++
@@ -1,7 +0,0 @@
-var AppWithErrorBoundary = () => (<ErrorBoundary><App /></ErrorBoundary>);
-
-var root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppWithErrorBoundary />);
-
-// ⚡ Hide loading screen as soon as React renders
-if (typeof window._hideLoader === 'function') window._hideLoader();
