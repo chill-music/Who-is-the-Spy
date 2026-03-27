4dbfc6b324fb38819742dc0885f154791676f998
chillmusic <37777162+chill-music@users.noreply.github.com>
2026-03-27 22:27:35 +0200

    Add files via upload

--- src/10-app.js
+++ src/10-app.js
@@ -7,58 +7,27 @@
     } = window;
 
     function App() {
-        // Use the new extracted state hook
         var state = window.useAppState();
-
         var { 
             lang, user, userData, isLoggedIn, isGuest, currentUID,
             notification, showOnboarding, onboardingGoogleUser,
             handleCreateGame, handleJoinGame, handleLeaveRoom, startGame, 
-            sendGameMessage, handleOnboardingComplete, handleGoogleLogin, 
+            sendMessage, handleOnboardingComplete, handleGoogleLogin, 
             handleClaimLoginReward, resetGame, handlePurchase, handleEquip, 
             handleUnequip, handleBuyVIP, handleSendProposal, handleAcceptProposal, 
             handleDeclineProposal, openProfile, handleSendGiftToUser, handleLogout, 
             closePrivateChat, createNotification, markNotificationRead, 
             clearAllNotifications, handleNotificationClick, handleAddFriendById, 
             handleAcceptRequest, handleRejectRequest, formatBanExpiry,
-            activeRooms, submitVote, submitWordVote
+            activeRooms, submitVote, submitWordVote,
+            handleSkipTurn, nextTurn, requestVoting, agreeToVote, declineVote,
+            triggerVoting, submitSpyWordGuess, submitMrWhiteLocationGuess, spyVoluntaryDeclare,
+            addBotToRoom, removeBotFromRoom, openPrivateChat, openProfile
         } = state;
 
-    
         return (
             <window.AppView
                 {...state}
-                handleCreateGame={handleCreateGame}
-                handleJoinGame={handleJoinGame}
-                handleLeaveRoom={handleLeaveRoom}
-                startGame={startGame}
-                sendGameMessage={sendGameMessage}
-                handleOnboardingComplete={handleOnboardingComplete}
-                handleGoogleLogin={handleGoogleLogin}
-                handleClaimLoginReward={handleClaimLoginReward}
-                resetGame={resetGame}
-                handlePurchase={handlePurchase}
-                handleEquip={handleEquip}
-                handleUnequip={handleUnequip}
-                handleBuyVIP={handleBuyVIP}
-                handleSendProposal={handleSendProposal}
-                handleAcceptProposal={handleAcceptProposal}
-                handleDeclineProposal={handleDeclineProposal}
-                openProfile={openProfile}
-                handleSendGiftToUser={handleSendGiftToUser}
-                handleLogout={handleLogout}
-                closePrivateChat={closePrivateChat}
-                createNotification={createNotification}
-                markNotificationRead={markNotificationRead}
-                clearAllNotifications={clearAllNotifications}
-                handleNotificationClick={handleNotificationClick}
-                handleAddFriendById={handleAddFriendById}
-                handleAcceptRequest={handleAcceptRequest}
-                handleRejectRequest={handleRejectRequest}
-                formatBanExpiry={formatBanExpiry}
-                activeRooms={activeRooms}
-                submitVote={submitVote}
-                submitWordVote={submitWordVote}
             />
         );
     }
